1f9955e20e59e91ed7b4c86070b67dcc30f19928
牛加人等于朱 <niuren.zhu@icloud.com>
2017-01-13 17:56:49 +0800

    集合添加的方法在实例中不存在？？

--- src/bobas/bobas.ts
+++ src/bobas/bobas.ts
@@ -1,7 +1,7 @@
 /**
  * 模块索引文件，此文件集中导出类
  */
-export * from './data/data';
-export * from './core/core';
-export * from './bo/bo';
-export * from './messages/messages';
+export * from './data/Data';
+export * from './core/Core';
+export * from './bo/BO';
+export * from './messages/Messages';
--- test/bobas/bo/testBO.ts
+++ test/bobas/bo/testBO.ts
@@ -1,6 +1,13 @@
 import * as bobas from '../../../src/bobas/bobas';
 import * as test from './SalesOrder';
 
+var arrays = new bobas.ArrayList();
+arrays.add("");
+let items = new test.SalesOrderItems();
+items.add(new test.SalesOrderItem());
+items.create();
+
+
 let order = new test.SalesOrder();
 order.Customer = "C00001";
 let item = order.Items.create();
@@ -13,4 +20,4 @@ item.ItemCode = "A00002";
 item.Price = 0.99;
 item.Quantity = 20;
 item.LineTotal = item.Price * item.Quantity;
-bobas.logger.log(bobas.emMessageLevel.DEBUG, order.toString());
+//bobas.logger.log(bobas.emMessageLevel.DEBUG, order.toString());
