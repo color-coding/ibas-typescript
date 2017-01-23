650786ac5c326d47cd3bf55f568e90d7b49298c9
牛加人等于朱 <niuren.zhu@icloud.com>
2017-01-23 16:43:13 +0800

    修改脚本支持参数（编译路径）

--- build_all.bat
+++ build_all.bat
@@ -9,7 +9,14 @@ echo     1. 此脚本需要在Node.js command prompt下运行。
 echo     2. 遍历当前目录下所有子目录，存在tsconfig.json则编译。
 echo ****************************************************************************
 REM 设置参数变量
-SET WORK_FOLDER=%~dp0
+REM 启动目录
+SET STARTUP_FOLDER=%~dp0
+REM 传入的工作目录
+SET WORK_FOLDER=%~1
+REM 判断是否传工作目录，没有则是启动目录
+if "%WORK_FOLDER%"=="" SET WORK_FOLDER=%STARTUP_FOLDER%
+REM 若工作目录最后字符不是“\”则补齐
+if "%WORK_FOLDER:~-0,1%" neq "\" SET WORK_FOLDER=%WORK_FOLDER%\
 
 for /f %%l in ('dir /s /b "%WORK_FOLDER%tsconfig.json"') DO (
   echo --开始编译：%%~dpl
@@ -18,4 +25,4 @@ REM 改变工作目录
   call tsc
 )
 REM 重置运行目录
-cd /d %WORK_FOLDER%
+cd /d %STARTUP_FOLDER%
