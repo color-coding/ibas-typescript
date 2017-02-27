7165c28f6e9a1cb139391a6439fe1a29fbca223d
牛加人等于朱 <niuren.zhu@icloud.com>
2017-02-27 16:00:35 +0800

    调整

--- apps/shell/borep/BORepositories.ts
+++ apps/shell/borep/BORepositories.ts
@@ -66,21 +66,21 @@ export class BORepositoryShell extends ibas.BORepositoryApplication implements s
 export class BORepositoryShellOffLine extends BORepositoryShell {
 
     /** 获取离线配置 */
-    private fetchOfflineSettings(callBack: Function): void {
+    private fetchSettings(caller: ibas.MethodCaller): void {
         this.address = document.location.href;
         let method: string = "config.json";
-        let caller: ibas.MethodCaller = {
+        let remotecaller: ibas.MethodCaller = {
             onCompleted(settings: any): void {
-                if (!ibas.object.isNull(callBack)) {
+                if (!ibas.object.isNull(caller.onCompleted)) {
                     let offlineSettings: any = null;
                     if (!ibas.object.isNull(settings) && !ibas.object.isNull(settings.offlineSettings)) {
                         offlineSettings = settings.offlineSettings;
                     }
-                    callBack.call(callBack, offlineSettings);
+                    caller.onCompleted.call(caller, offlineSettings);
                 }
             }
         };
-        this.callRemoteMethod(method, null, caller);
+        this.callRemoteMethod(method, null, remotecaller);
     }
     private offlineConverter: DataConverter4Offline;
     /**
@@ -98,29 +98,30 @@ export class BORepositoryShellOffLine extends BORepositoryShell {
 	 * @param caller 登录者
 	 */
     connect(caller: sys.ConnectCaller): void {
-        let callBack = function (offlineSettings: any): void {
-            let opRslt = new ibas.OperationResult();
-            opRslt.resultCode = -1;
-            opRslt.message = ibas.i18n.prop("sys_shell_user_and_password_not_match");
-            if (!ibas.object.isNull(offlineSettings)
-                && !ibas.object.isNull(offlineSettings.users)
-                && Array.isArray(offlineSettings.users)) {
-                for (let item of offlineSettings.users) {
-                    if (item.user === caller.user
-                        && item.password === caller.password) {
-                        opRslt.resultCode = 0;
-                        opRslt.message = "";
-                        let user: bo.User = new bo.User();
-                        user.userCode = item.user;
-                        user.userName = item.name;
-                        opRslt.resultObjects.add(user);
-                        break;
+        this.fetchSettings({
+            onCompleted(settings: any): void {
+                let opRslt = new ibas.OperationResult();
+                opRslt.resultCode = -1;
+                opRslt.message = ibas.i18n.prop("sys_shell_user_and_password_not_match");
+                if (!ibas.object.isNull(settings)
+                    && !ibas.object.isNull(settings.users)
+                    && Array.isArray(settings.users)) {
+                    for (let item of settings.users) {
+                        if (item.user === caller.user
+                            && item.password === caller.password) {
+                            opRslt.resultCode = 0;
+                            opRslt.message = "";
+                            let user: bo.User = new bo.User();
+                            user.userCode = item.user;
+                            user.userName = item.name;
+                            opRslt.resultObjects.add(user);
+                            break;
+                        }
                     }
                 }
+                caller.onCompleted.call(ibas.object.isNull(caller.caller) ? caller : caller.caller, opRslt);
             }
-            caller.onCompleted.call(ibas.object.isNull(caller.caller) ? caller : caller.caller, opRslt);
-        };
-        this.fetchOfflineSettings(callBack);
+        });
     }
 
 	/**
@@ -128,25 +129,26 @@ export class BORepositoryShellOffLine extends BORepositoryShell {
 	 * @param caller 用户检索者
 	 */
     fetchUserModules(caller: sys.UserMethodsCaller<sys.IUserModule>): void {
-        let callBack = function (offlineSettings: any): void {
-            let opRslt = new ibas.OperationResult();
-            if (!ibas.object.isNull(offlineSettings)
-                && !ibas.object.isNull(offlineSettings.modules)
-                && Array.isArray(offlineSettings.modules)) {
-                for (let item of offlineSettings.modules) {
-                    let module = new bo.UserModule();
-                    module.id = item.id;
-                    module.name = item.name;
-                    module.category = item.category;
-                    module.address = item.address;
-                    module.console = item.console;
-                    module.description = ibas.i18n.prop(module.name);
-                    opRslt.resultObjects.add(module);
+        this.fetchSettings({
+            onCompleted(settings: any): void {
+                let opRslt = new ibas.OperationResult();
+                if (!ibas.object.isNull(settings)
+                    && !ibas.object.isNull(settings.modules)
+                    && Array.isArray(settings.modules)) {
+                    for (let item of settings.modules) {
+                        let module = new bo.UserModule();
+                        module.id = item.id;
+                        module.name = item.name;
+                        module.category = item.category;
+                        module.address = item.address;
+                        module.console = item.console;
+                        module.description = ibas.i18n.prop(module.name);
+                        opRslt.resultObjects.add(module);
+                    }
                 }
+                caller.onCompleted.call(ibas.object.isNull(caller.caller) ? caller : caller.caller, opRslt);
             }
-            caller.onCompleted.call(ibas.object.isNull(caller.caller) ? caller : caller.caller, opRslt);
-        };
-        this.fetchOfflineSettings(callBack);
+        });
     }
 
 
