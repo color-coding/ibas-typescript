cf5afabc5c2ce9faf2e8bac362a3a8ecf9595fc1
牛加人等于朱 <niuren.zhu@icloud.com>
2017-07-28 13:17:27 +0800

    调整声明类型

--- test/apps/trainingtesting/bsapp/Console.ts
+++ test/apps/trainingtesting/bsapp/Console.ts
@@ -65,7 +65,7 @@ export class Console extends ibas.ModuleConsole {
             // 使用c类型视图
             uiModules.push("../bsui/c/Navigation");
         }
-        let that: Console = this;
+        let that: this = this;
         require(uiModules, function (ui: any): void {
             // 设置导航
             that._navigation = new ui.default();
