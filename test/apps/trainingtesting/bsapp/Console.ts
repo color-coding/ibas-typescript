8ed26803ecfedc74bf88ec7a33381a7559aae14e
neilzhou0309 <neil.zhou@avatech.com.cn>
2017-08-17 14:22:05 +0800

    百度地图Demo

--- test/apps/trainingtesting/bsapp/Console.ts
+++ test/apps/trainingtesting/bsapp/Console.ts
@@ -10,7 +10,7 @@ import * as ibas from "ibas/index";
 import { MaterialFunc, MaterialChooseServiceMapping, MaterialLinkServiceMapping } from "./material/index";
 import { CustomerFunc, CustomerChooseServiceMapping, CustomerLinkServiceMapping } from "./customer/index";
 import { SalesOrderFunc, SalesOrderChooseServiceMapping, SalesOrderLinkServiceMapping } from "./salesorder/index";
-import { DemoUrlFunc, DemoTabFunc, DemoResidentApp, DemoServiceMapping, DemoMapFunc } from "./others/index";
+import { DemoUrlFunc, DemoTabFunc, DemoResidentApp, DemoServiceMapping, DemoMapFunc ,PageDemoFunc} from "./others/index";
 
 /** 模块控制台 */
 export class Console extends ibas.ModuleConsole {
@@ -100,6 +100,7 @@ export class ConsoleOthers extends ibas.ModuleConsole {
         this.register(new DemoUrlFunc());
         this.register(new DemoTabFunc());
         this.register(new DemoMapFunc());
+        this.register(new PageDemoFunc());
         // 注册服务应用
         this.register(new DemoServiceMapping());
         // 注册常驻应用
@@ -128,7 +129,15 @@ export class ConsoleOthers extends ibas.ModuleConsole {
             that._navigation = new ui.default();
             // 调用初始化
             that.initialize();
+            //加载百度地址资源
+            domScript = document.createElement('script');
+            domScript.src = "http://api.map.baidu.com/getscript?v=2.0&ak=0tAfPNP8bYG9ZUUkkvgu8Fv48ng9uFfl&services=&t=20170803155555";
+            document.getElementsByTagName('head')[0].appendChild(domScript);
+            var domScript = document.createElement('script');
+            domScript.src = "http://api.map.baidu.com/api?v=2.0&ak=0tAfPNP8bYG9ZUUkkvgu8Fv48ng9uFfl";
+            document.getElementsByTagName('head')[0].appendChild(domScript);
         });
+
         // 保留基类方法
         super.run();
     }
--- test/apps/trainingtesting/bsui/c/others/DemoMapView.ts
+++ test/apps/trainingtesting/bsui/c/others/DemoMapView.ts
@@ -9,19 +9,92 @@
 import * as ibas from "ibas/index";
 import { utils } from "openui5/typings/ibas.utils";
 import { IDemoMapView } from "../../../bsapp/others/index";
-
+declare var BMap;
+declare var BMAP_ANCHOR_TOP_LEFT ;
+declare var BMAP_ANCHOR_TOP_RIGHT ;
 /**
  * 视图-地图
  */
 export class DemoMapView extends ibas.View implements IDemoMapView {
-
     /** 绘制视图 */
     darw(): any {
-        return new sap.ui.core.HTML("", {
-            content: "",
-            preferDOM: true,
-            sanitizeContent: false,
-            visible: true,
+        let lngtxt:sap.m.Input=new sap.m.Input("");
+        let lattxt:sap.m.Input=new sap.m.Input("");
+        let page: sap.m.Page = new sap.m.Page("", {
+            showHeader: false,
+            subHeader: new sap.m.Toolbar("", {
+                content: [
+                    new sap.m.Button("", {
+                        text: "加载地图",
+                        type: sap.m.ButtonType.Transparent,
+                        icon: "sap-icon://synchronize",
+                        press: function (event: any): void {
+                            var map = new BMap.Map('map');
+                            var point = new BMap.Point(116.404, 38.915);
+
+                            map.centerAndZoom(point, 14);
+                            var pt = new BMap.Point(116.404, 39.913);
+                            //var myIcon = new BMap.Icon("http://developer.baidu.com/map/jsdemo/img/fox.gif", new BMap.Size(300, 157));
+                            //var marker = new BMap.Marker(pt, { icon: myIcon });
+                            var marker = new BMap.Marker(pt);
+                            marker.enableDragging()
+                            map.addOverlay(marker);
+                            map.enableScrollWheelZoom(true);
+
+                            marker.addEventListener("click",attribute);
+                            function attribute(e){
+                                var p = e.target;
+                                lngtxt.setValue(p.getPosition().lng);
+                                lattxt.setValue(p.getPosition().lat);
+                                //alert("marker的位置是" + p.getPosition().lng + "," + p.getPosition().lat);
+                            }
+
+                            var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});// 左上角，添加比例尺
+                            var top_left_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_LEFT});  //左上角，添加默认缩放平移控件
+                            map.addControl(top_left_control);
+                            map.addControl(top_left_navigation);
+
+                            var size = new BMap.Size(10, 20);
+
+                            map.addControl(new BMap.CityListControl({
+                                anchor: BMAP_ANCHOR_TOP_RIGHT,
+                                offset: size,
+                                // 切换城市之间事件
+                                // onChangeBefore: function(){
+                                //    alert('before');
+                                // },
+                                // 切换城市之后事件
+                                // onChangeAfter:function(){
+                                //   alert('after');
+                                // }
+                            }));
+
+                            var geolocation = new BMap.Geolocation();
+                            geolocation.getCurrentPosition(function(r){
+                                if(this.getStatus() == 0){
+                                    var mk = new BMap.Marker(r.point);
+                                    map.addOverlay(mk);
+                                    map.panTo(r.point);
+                                    //alert('您的位置：'+r.point.lng+','+r.point.lat);
+                                }
+                                else {
+                                    alert('failed'+this.getStatus());
+                                }
+                            },{enableHighAccuracy: true})
+                        }
+                    }),
+                    lngtxt,
+                    lattxt
+                ],
+
+            }),
+            content: [
+                new sap.ui.core.HTML("", {
+                    content: "<div  id='map' style='width:100%;height:100%'></div>",
+                })
+            ]
         });
+
+        return page
     }
 }
