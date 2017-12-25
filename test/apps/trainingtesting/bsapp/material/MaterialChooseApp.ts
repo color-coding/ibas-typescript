16fc617c201e9993b2c832209e9a93bd92a9ae9b
牛加人等于朱 <niuren.zhu@icloud.com>
2017-12-25 12:01:23 +0800

    修复ChooseApp分页时自动选择问题

--- test/apps/trainingtesting/bsapp/material/MaterialChooseApp.ts
+++ test/apps/trainingtesting/bsapp/material/MaterialChooseApp.ts
@@ -50,7 +50,7 @@ export class MaterialChooseApp extends ibas.BOChooseService<IMaterialChooseView,
                         throw new Error(opRslt.message);
                     }
                     if (opRslt.resultObjects.length === 1
-                        && ibas.config.get(ibas.CONFIG_ITEM_AUTO_CHOOSE_DATA, true)) {
+                        && ibas.config.get(ibas.CONFIG_ITEM_AUTO_CHOOSE_DATA, true) && !that.isViewShowed()) {
                         // 仅一条数据，直接选择
                         that.chooseData(opRslt.resultObjects);
                     } else {
