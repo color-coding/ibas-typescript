7ed66a2bece15d7c75c67c433d867fe6bd6d5e5f
牛加人等于朱 <niuren.zhu@icloud.com>
2018-01-03 14:10:00 +0800

    添加页面

---
+++ test/apps/trainingtesting/bsui/m/others/DemoResidentView.ts
@@ -0,0 +1,17 @@
+﻿/**
+ * @license
+ * Copyright color-coding studio. All Rights Reserved.
+ *
+ * Use of this source code is governed by an Apache License, Version 2.0
+ * that can be found in the LICENSE file at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import * as ibas from "ibas/index";
+import * as openui5 from "openui5/index";
+import { DemoResidentView as cDemoResidentView } from "../../c/others/index";
+
+/**
+ * 视图-常驻
+ */
+export class DemoResidentView extends cDemoResidentView {
+}
