e0d1f20e96206c193f5f8c2c465278ea8ed2cb2b
牛加人等于朱 <niuren.zhu@icloud.com>
2018-01-18 12:06:16 +0800

    调整选择页面表格选择风格

--- test/apps/trainingtesting/bsui/c/salesorder/SalesOrderChooseView.ts
+++ test/apps/trainingtesting/bsui/c/salesorder/SalesOrderChooseView.ts
@@ -86,7 +86,8 @@ export class SalesOrderChooseView extends ibas.BOChooseView implements ISalesOrd
                 })
             ]
         });
-        this.id = this.table.getId();
+        // 调整选择样式风格
+        openui5.utils.changeSelectionStyle(this.table, this.chooseType);
         // 添加列表自动查询事件
         openui5.utils.triggerNextResults({
             listener: this.table,
