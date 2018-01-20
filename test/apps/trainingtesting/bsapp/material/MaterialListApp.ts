2503745f82c82892b8a24c9ea997c53a8fcde72e
niurenzhu <niuren.zhu@icloud.com>
2018-01-20 16:28:59 +0800

    提示选择删除的数据

--- test/apps/trainingtesting/bsapp/material/MaterialListApp.ts
+++ test/apps/trainingtesting/bsapp/material/MaterialListApp.ts
@@ -122,6 +122,9 @@ export class MaterialListApp extends ibas.BOListApplication<IMaterialListView, b
         }
         // 没有选择删除的对象
         if (beDeleteds.length === 0) {
+            this.messages(ibas.emMessageType.WARNING, ibas.i18n.prop("shell_please_chooose_data",
+                ibas.i18n.prop("shell_data_delete")
+            ));
             return;
         }
         let that: this = this;
