c810e7104a0bfe23ca2887aa6796fd86299a1202
牛加人等于朱 <niuren.zhu@icloud.com>
2018-01-30 11:28:59 +0800

    梳理业务对象及规则

--- test/apps/trainingtesting/api/bo/SalesOrder.d.ts
+++ test/apps/trainingtesting/api/bo/SalesOrder.d.ts
@@ -132,7 +132,7 @@ export interface ISalesOrder extends IBODocument {
     /** 单据货币 */
     documentCurrency: string;
 
-    /** 单据交换率 */
+    /** 单据汇率 */
     documentRate: number;
 
     /** 单据总计 */
