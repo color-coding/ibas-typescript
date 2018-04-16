7a331daf6757509dc8b20a7642c3f8b6f4ecf63d
neilzhou0309 <neil.zhou@avatech.com.cn>
2018-04-16 15:20:27 +0800

    sap.ui.table添加两个方法描述

--- openui5/types/sap.ui.table.d.ts
+++ openui5/types/sap.ui.table.d.ts
@@ -44,6 +44,8 @@ declare namespace sap {
                 setFooter(footer: sap.ui.core.Control | string): sap.ui.table.Table;
                 attachRowSelectionChange(oData?, fnFunction?, oListener?): sap.ui.table.Table;
                 setSelectionMode(sSelectionMode: sap.ui.table.SelectionMode);
+                setSelectedIndex(iSelectedIndex): sap.ui.table.Table;
+                addSelectionInterval(iIndexFrom, iIndexTo): sap.ui.table.Table
             }
             export class Column extends sap.ui.core.Control {
                 constructor(mSettings?: any);
