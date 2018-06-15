649cd7e6341a56e356e2544cf74fe04dee4fd115
neilzhou0309 <neil.zhou@avatech.com.cn>
2018-06-15 15:01:36 +0800

    添加浏览器缓存

--- openui5/extends/ibas.ex.ts
+++ openui5/extends/ibas.ex.ts
@@ -6,11 +6,16 @@
  * that can be found in the LICENSE file at http://www.apache.org/licenses/LICENSE-2.0
  */
 namespace openui5 {
+    /** 配置项目-是否应用web缓存 */
+    export const CONFIG_ITEM_WEB_CACHE: string = "webcache";
+    /** 配置项目-weby库名 */
+    export const CONFIG_ITEM_WEB_CACHE_DB_NAME: string = "webcachedbname";
     export class BORepsitory {
         rootUrl: string = ibas.urls.rootUrl("/openui5/index");
         keyAttribute: string;
         textAttribute: string;
         boName: string;
+        boCode: string;
         repositoryName: string;
         childPropertyName: string;
         selectedKey: string;
@@ -18,8 +23,10 @@ namespace openui5 {
         provinces: Array<any>;
         citys: Array<any>;
         districts: Array<any>;
+        cache: boolean = false;
+        private systemWebCache: boolean = ibas.config.get(CONFIG_ITEM_WEB_CACHE, false);
         constructor(repositoryName?: string, boName?: string, keyAttribute?: string,
-            textAttribute?: string, criteria?: ibas.Criteria, selectedKey?: string,childPropertyName?:string) {
+            textAttribute?: string, criteria?: ibas.Criteria, selectedKey?: string, childPropertyName?: string) {
             this.repositoryName = repositoryName;
             this.boName = boName;
             this.keyAttribute = keyAttribute;
@@ -30,11 +37,27 @@ namespace openui5 {
         }
         async getKeyTextList(): Promise<ibas.ArrayList<ibas.KeyText>> {
             let that: this = this;
+            let ibasDB: openui5.IbasDB;
+            let cacheKeyText: ibas.KeyText;
+            if (that.cache && that.systemWebCache) {
+                ibasDB = await openui5.IbasDB.getIndexedDB();
+                if (!ibas.objects.isNull(ibasDB)) {
+                    cacheKeyText = await ibasDB.getData(that.boCode, that.selectedKey);
+                }
+            }
             let promise: Promise<ibas.ArrayList<ibas.KeyText>> = new Promise<ibas.ArrayList<ibas.KeyText>>(resolve => {
                 if (ibas.strings.isEmpty(that.repositoryName) || ibas.strings.isEmpty(that.boName)
                     || ibas.strings.isEmpty(that.keyAttribute) || ibas.strings.isEmpty(that.textAttribute)
                     || !that.criteria) {
                     resolve(null);
+                }
+                if (that.cache && that.systemWebCache) {
+                    if (!!cacheKeyText) {
+                        let keyTextList: ibas.ArrayList<ibas.KeyText> = new ibas.ArrayList<ibas.KeyText>();
+                        keyTextList.push(cacheKeyText);
+                        resolve(keyTextList);
+                        return;
+                    }
                 }
                 let boRep: any = ibas.boFactory.create(this.repositoryName);
                 let criteria: ibas.ICriteria = new ibas.Criteria();
@@ -49,6 +72,9 @@ namespace openui5 {
                                 keyText.key = data.getProperty(that.keyAttribute);
                                 keyText.text = data.getProperty(that.textAttribute);
                                 keyTextList.push(keyText);
+                                if (that.cache && that.systemWebCache && !ibas.objects.isNull(ibasDB)) {
+                                    ibasDB.add(that.boCode, keyText);
+                                }
                             }
                             resolve(keyTextList);
                         } else {
@@ -126,7 +152,7 @@ namespace openui5 {
             }
             let url: string = ibas.strings.format("{0}/data/district.json", this.rootUrl);
             this.districts = await this.load(url);
-            this.addLocalStorage("districts",this.districts);
+            this.addLocalStorage("districts", this.districts);
             if (ibas.objects.isNull(this.districts)) {
                 return false;
             } else {
@@ -143,11 +169,11 @@ namespace openui5 {
                     async: false,
                     cache: false,
                     error: function (xhr: JQueryXHR, status: string, error: string): void {
-                        ibas.logger.log(ibas.emMessageLevel.DEBUG,ibas.strings.format("config: load file [{2}] faild [{0} - {1}].", status, error, address));
+                        ibas.logger.log(ibas.emMessageLevel.DEBUG, ibas.strings.format("config: load file [{2}] faild [{0} - {1}].", status, error, address));
                         resolve(null);
                     },
                     success: function (data: any): void {
-                        ibas.logger.log(ibas.emMessageLevel.DEBUG,ibas.strings.format("config: load file [{0}] successful.", address));
+                        ibas.logger.log(ibas.emMessageLevel.DEBUG, ibas.strings.format("config: load file [{0}] successful.", address));
                         resolve(data);
                     },
                 };
@@ -161,5 +187,218 @@ namespace openui5 {
         getLocalStorage(name: string): any {
             return JSON.parse(window.localStorage.getItem(name));
         }
+    }
+    /**
+     * 缓存库 操作类
+     */
+    export class IbasDB {
+        private ibasDBInfo: IbasDBInfo;
+        /** web indexed库名 */
+        private static webDBName: string = ibas.config.get(CONFIG_ITEM_WEB_CACHE_DB_NAME, "ibas_db");
+
+        private static _indexedDB: openui5.IbasDB;
+        public static async  getIndexedDB(): Promise<openui5.IbasDB> {
+            // 判断浏览器是否支持indexedDB
+            if (!window.indexedDB) {
+                return null;
+            }
+            if (!IbasDB._indexedDB) {
+                IbasDB._indexedDB = new openui5.IbasDB();
+                IbasDB._indexedDB.ibasDBInfo = new IbasDBInfo();
+                IbasDB._indexedDB.ibasDBInfo.dbName = IbasDB.webDBName;
+                IbasDB._indexedDB.ibasDBInfo.currentDBFactory = window.indexedDB;
+                await IbasDB._indexedDB.initDB();
+            }
+            return IbasDB._indexedDB;
+        }
+        /**
+         * 初始化DB
+         * @param version 数据库版本，缺省则打开当前版本
+         * @param newTables 需要初始化的表
+         */
+        public async initDB(version?: number, newTables?: Array<string>): Promise<boolean> {
+            let that: this = this;
+            let promise: Promise<boolean> = new Promise<boolean>(resolve => {
+                let dbRequest: IDBOpenDBRequest = !version ? that.ibasDBInfo.currentDBFactory.open(that.ibasDBInfo.dbName)
+                    : that.ibasDBInfo.currentDBFactory.open(that.ibasDBInfo.dbName, version);
+                dbRequest.onerror = function (e: any): void {
+                    console.log("OPen Error!");
+                };
+                dbRequest.onupgradeneeded = function (e: any): void {
+                    that.ibasDBInfo.currentDB = e.target.result;
+                    if (!!newTables) {
+                        for (let table of newTables) {
+                            if (!that.ibasDBInfo.currentDB.objectStoreNames.contains(table)) {
+                                let store: IDBObjectStore = that.ibasDBInfo.currentDB.createObjectStore(table, { keyPath: "key" });
+                                store.createIndex("textIndex", "text", { unique: true });
+                                store.createIndex("keyIndex", "key", { unique: true });
+                            }
+                        }
+                    }
+                };
+                dbRequest.onsuccess = function (e: any): void {
+                    that.ibasDBInfo.currentDB = e.target.result;
+                    that.ibasDBInfo.dbVersion = that.ibasDBInfo.currentDB.version;
+                    resolve(true);
+                };
+            });
+            return promise;
+        }
+        /**
+         * 删除数据库
+         * @param dbName 数据库名称
+         */
+        deleteDB(dbName?: string): void {
+            // 未传入表名,默认为配置文件中表名
+            if (!dbName) {
+                dbName = ibas.config.get(CONFIG_ITEM_WEB_CACHE_DB_NAME, "ibas_db");
+            }
+            this.ibasDBInfo.currentDBFactory.deleteDatabase(dbName);
+        }
+        /**
+         * 获取读写权限
+         * @param storeName 表名
+         */
+        async getStore(storeName: string, readOnly?: boolean): Promise<IDBObjectStore> {
+            try {
+                // 如果当前缓存中不存在该表则重新初始化数据库
+                if (!this.ibasDBInfo.currentDB.objectStoreNames.contains(storeName)) {
+                    let newTables: Array<string> = new Array<string>();
+                    newTables.push(storeName);
+                    this.ibasDBInfo.currentDB.close();
+                    await this.initDB(this.ibasDBInfo.dbVersion + 1, newTables);
+                }
+                let transaction: IDBTransaction = (!readOnly) ?
+                    this.ibasDBInfo.currentDB.transaction(storeName, "readwrite") : this.ibasDBInfo.currentDB.transaction(storeName, "readonly");
+                return transaction.objectStore(storeName);
+            } catch (error) {
+                ibas.logger.log(ibas.emMessageLevel.DEBUG, ibas.i18n.prop("sap_ibas_ex_ibasdb_not_store"));
+                return null;
+            }
+        }
+        /**
+         * 添加记录
+         * @param storeName 表名
+         */
+        async add(storeName: string, data: ibas.KeyText): Promise<string> {
+            let store: IDBObjectStore = await this.getStore(storeName);
+            let promise: Promise<string> = new Promise<string>(resolve => {
+                let request: IDBRequest = store.add(data);
+                request.onsuccess = function (e: any): void {
+                    let key: string = e.target.result;
+                    resolve(key);
+                };
+                request.onerror = function (e: any): void {
+                    resolve(null);
+                };
+            });
+            return promise;
+        }
+        /**
+         * 更新记录
+         * @param storeName 表名
+         * @param updateKey 更新记录主键
+         */
+        async update(storeName: string, newData: ibas.KeyText): Promise<ibas.KeyText> {
+            let store: IDBObjectStore = await this.getStore(storeName);
+            let promise: Promise<ibas.KeyText> = new Promise<ibas.KeyText>(resolve => {
+                let request: IDBRequest = store.get(newData.key);
+                request.onsuccess = function (e: any): void {
+                    let obj: ibas.KeyText = e.target.result;
+                    obj.text = newData.text;
+                    store.put(obj);
+                    resolve(obj);
+                };
+                request.onerror = function (e: any): void {
+                    resolve(null);
+                };
+            });
+            return promise;
+        }
+        /**
+         * 删除记录
+         * @param storeName 表名
+         * @param updateKey 删除记录主键
+         */
+        async delete(storeName: string, deleteKey: string): Promise<boolean> {
+            let store: IDBObjectStore = await this.getStore(storeName);
+            let promise: Promise<boolean> = new Promise<boolean>(resolve => {
+                let request: IDBRequest = store.delete(deleteKey);
+                request.onsuccess = function (e: any): void {
+                    resolve(true);
+                };
+                request.onerror = function (e: any): void {
+                    resolve(false);
+                };
+            });
+            return promise;
+        }
+        /**
+         * 清除表中所有记录
+         * @param storeName 表名
+         */
+        async clearObjectStore(storeName: string): Promise<void> {
+            let store: IDBObjectStore = await this.getStore(storeName);
+            store.clear();
+        }
+        /**
+         * 查询
+         * @param storeName 表名
+         * @param fetchKey 查询主键
+         */
+        async fetch(storeName: string, fetchKey: string): Promise<Array<ibas.KeyText>> {
+            let store: IDBObjectStore = await this.getStore(storeName,true);
+            let key: IDBIndex = store.index("textIndex");
+            let request: IDBRequest = key.openCursor(IDBKeyRange.only(fetchKey));
+            let results: Array<ibas.KeyText> = new Array<ibas.KeyText>();
+            let promise: Promise<Array<ibas.KeyText>> = new Promise<Array<ibas.KeyText>>(resolve => {
+                request.onsuccess = function (e: any): void {
+                    let cursor: any = e.target.result;
+                    if (cursor) {
+                        let keytext: ibas.KeyText = cursor.value;
+                        results.push(keytext);
+                        cursor.continue();
+                    } else {
+                        resolve(results);
+                    }
+                };
+                request.onerror = function (e: any): void {
+                    resolve(null);
+                };
+            });
+            return promise;
+        }
+        /**
+         * 查询
+         * @param storeName 表名
+         * @param indexValue 查询值
+         */
+        async getData(storeName: string, indexValue: string): Promise<ibas.KeyText> {
+            let store: IDBObjectStore = await this.getStore(storeName,true);
+            let promise: Promise<ibas.KeyText> = new Promise<ibas.KeyText>(resolve => {
+                let index: IDBIndex = store.index("keyIndex");
+                index.get(indexValue).onsuccess = function (e: any): void {
+                    let obj: ibas.KeyText = e.target.result;
+                    resolve(obj);
+                };
+                index.get(indexValue).onerror = function (e: any): void {
+                    resolve(null);
+                };
+            });
+            return promise;
+        }
+    }
+    /**
+     * 缓存库 操作类信息
+     */
+    export class IbasDBInfo {
+        /** 库名 */
+        dbName: string;
+        /** 版本号 */
+        dbVersion: number;
+        /** 库工厂 */
+        currentDBFactory: IDBFactory;
+        /** 当前操作库实例 */
+        currentDB: IDBDatabase;
     }
 }
