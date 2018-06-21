0b1dd7143b966b0dcb61ec68b8e26b6a497d2f80
牛加人等于朱 <niuren.zhu@icloud.com>
2018-06-21 13:54:38 +0800

    完善本地存储

--- ibas/bobas/repository/BORepositoryLocal.ts
+++ ibas/bobas/repository/BORepositoryLocal.ts
@@ -49,6 +49,7 @@ namespace ibas {
             dbRequest.onupgradeneeded = function (e: Event): void {
                 that.db = (<any>e).target.result;
                 opener.onSuccess(that.db);
+                dbRequest.onsuccess = null;
             };
         }
         /** 关闭数据库 */
@@ -152,32 +153,9 @@ namespace ibas {
             if (strings.isEmpty(boName)) {
                 boName = "buckets";
             }
-            let storeParameters: IDBObjectStoreParameters = null;
-            if (caller.beSaved instanceof BODocument) {
-                storeParameters = {
-                    keyPath: "DocEntry"
-                };
-            } else if (caller.beSaved instanceof BODocumentLine) {
-                storeParameters = {
-                    keyPath: ["DocEntry", "LineId"]
-                };
-            } else if (caller.beSaved instanceof BOMasterData) {
-                storeParameters = {
-                    keyPath: "Code"
-                };
-            } else if (caller.beSaved instanceof BOMasterDataLine) {
-                storeParameters = {
-                    keyPath: ["Code", "LineId"]
-                };
-            } else if (caller.beSaved instanceof BOSimple) {
-                storeParameters = {
-                    keyPath: "ObjectKey"
-                };
-            } else if (caller.beSaved instanceof BOSimpleLine) {
-                storeParameters = {
-                    keyPath: ["ObjectKey", "LineId"]
-                };
-            }
+            let storeParameters: IDBObjectStoreParameters = {
+                autoIncrement: true,
+            };
             let that: this = this;
             this.openDB({
                 onError(error: Error): void {
