eca1feb36f52d7a19b3d8aa8d520d728ad0e3c18
Niuren.Zhu <niuren.zhu@icloud.com>
2025-08-01 18:21:13 +0800

    优化代码

--- openui5/extensions/components/Table.m.ts
+++ openui5/extensions/components/Table.m.ts
@@ -18,6 +18,13 @@ namespace sap {
                         chooseType: { type: "int", defaultValue: ibas.emChooseType.MULTIPLE },
                     },
                     events: {
+                        "nextDataSet": {
+                            parameters: {
+                                data: {
+                                    type: "any",
+                                }
+                            }
+                        }
                     }
                 },
                 renderer: {},
@@ -100,6 +107,44 @@ namespace sap {
                         */
                     }
                     return sap.m.Table.prototype.applySettings.apply(this, arguments);
+                },
+                init(this: Table): void {
+                    // 基类初始化
+                    (<any>sap.m.Table.prototype).init.apply(this, arguments);
+                    // 监听行变化事件
+                    this.attachEvent("updateFinished", undefined, (event: sap.ui.base.Event) => {
+                        if (!this.hasListeners("nextDataSet")) {
+                            // 没有注册事件，则退出
+                            return;
+                        }
+                        if (this.getBusy()) {
+                            // 忙状态不监听
+                            return;
+                        }
+                        if (event.getParameter("reason") !== "Growing") {
+                            // 非滚动条原因，不触发
+                            return;
+                        }
+                        let model: any = this.getModel(undefined);
+                        if (!ibas.objects.isNull(model)) {
+                            let data: any = model.getData();
+                            if (!ibas.objects.isNull(data) && !ibas.objects.isNull(this.getGrowingInfo())) {
+                                if (this.getGrowingInfo().total === this.getGrowingInfo().actual) {
+                                    if (!ibas.objects.isNull(data)) {
+                                        let modelData: any = data.rows; // 与绑定对象的路径有关
+                                        let dataCount: number = modelData instanceof Array ? modelData.length : 0;
+                                        let visibleRow: number = this.getGrowingThreshold(); // 当前显示条数
+                                        if (dataCount <= 0 || dataCount < visibleRow) {
+                                            return;
+                                        }
+                                        // 调用事件
+                                        this.setBusy(true);
+                                        this.fireNextDataSet({ data: modelData[modelData.length - 1] });
+                                    }
+                                }
+                            }
+                        }
+                    });
                 },
                 /** 退出 */
                 exit(this: Table): void {
